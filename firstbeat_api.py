5d24f26de62c0044fb3108944da8c5ce7b1365ff
clouchheim <carter.louchheim@example.com>
2025-12-16 12:11:33 -0700

    pulling responses from past day

--- firstbeat_api.py
+++ firstbeat_api.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
 
 # =========================
-# ENV + AUTH SETUP
+# ENV + AUTH SETUP + GLOBALS
 # =========================
 load_dotenv()
 
@@ -16,9 +16,15 @@ CONSUMER_ID = os.getenv("ID")
 SHARED_SECRET = os.getenv("SHARED_SECRET")
 API_KEY = os.getenv("API_KEY")
 
+TEAM_ID = 20168 # all MALP and WALP on Firstbeat
+LAST_X_DAYS = 1 # just from time of run back 24 hours (NOTE: data must be loaded into firstbeat cloud)
+USSS_COACH_ID = '3-4925' # U.S. Ski and Snowboard id
+
+# IF missing correct infomration (probably in .env file, raise error)
 if not CONSUMER_ID or not SHARED_SECRET or not API_KEY:
     raise RuntimeError("Missing ID, SHARED_SECRET, or API_KEY in .env")
 
+# GENERATE valid jwt key for header
 def generate_jwt():
     now = int(time.time())
     payload = {"iss": CONSUMER_ID, "iat": now, "exp": now + 300}
@@ -27,6 +33,7 @@ def generate_jwt():
         token = token.decode("utf-8")
     return token
 
+# define headers for all api calls
 def auth_headers():
     return {
         "Authorization": f"Bearer {generate_jwt()}",
@@ -34,12 +41,13 @@ def auth_headers():
         "Accept": "application/json"
     }
 
+# GET time bounds (from x days back, to present moment)
 def last_x_days_utc_range(days_back):
     today_utc = datetime.now(timezone.utc).date()
     past_utc = today_utc - timedelta(days=days_back)
 
-    to_time = datetime.combine(today_utc, datetime.min.time(), tzinfo=timezone.utc)
-    from_time   = datetime.combine(past_utc, datetime.max.time(), tzinfo=timezone.utc)
+    from_time = datetime.combine(past_utc, datetime.min.time(), tzinfo=timezone.utc)
+    to_time   = datetime.combine(today_utc, datetime.max.time(), tzinfo=timezone.utc)
 
     return (
         from_time.isoformat().replace("+00:00", "Z"),
@@ -47,7 +55,7 @@ def last_x_days_utc_range(days_back):
     )
 
 # =========================
-# HELPER FOR SAFE REQUESTS
+# HELPER FOR API REQUESTS
 # =========================
 def test_endpoint(name, url, params=None, max_retries=5):
     print(f"\n--- {name} ---")
@@ -63,76 +71,122 @@ def test_endpoint(name, url, params=None, max_retries=5):
             time.sleep(5)
             continue
         break
-
-    print("Response (first 500 chars):")
-    print(r.text[:500])
+    
     return r
+
+def get_measurement_ids(athlete_id, from_time, to_time, name=''):
+    measurement = test_endpoint( 
+        f"Measurements (athlete {name} ({athlete_id}))", 
+        f"{BASE_URL}/sports/accounts/{USSS_COACH_ID}/athletes/{athlete_id}/measurements/", 
+        params={"fromTime": from_time, "toTime": to_time})
+
+    # Get ids of measurements to pull results
+    measurement_ids = [
+        m["measurementId"]
+        for m in measurement.json().get("measurements", [])
+        if "measurementId" in m
+    ]
+
+    if len(measurement_ids) > 0:
+        print(f'\n-- Found {len(measurement_ids)} measurement for {name} --')
+
+    return measurement_ids
+
+def get_measurement_results(athlete_id, measurement_id):
+    resp = test_endpoint(
+        f"Measurement Results ({measurement_id}-{athlete_id})",
+        f"{BASE_URL}/sports/accounts/{USSS_COACH_ID}/athletes/{athlete_id}/measurements/{measurement_id}/results",
+        params={
+            "format": "list",
+            "var": "rmssd"
+        }
+    )
+
+    return resp.text
 
 # =========================
 # STEP 1 — ACCOUNTS
 # =========================
-accounts_resp = test_endpoint("Accounts", f"{BASE_URL}/sports/accounts")
+'''accounts_resp = test_endpoint("Accounts", f"{BASE_URL}/sports/accounts")
 if accounts_resp.status_code != 200:
     print("\n❌ Cannot access accounts — stop here.")
     exit()
-accounts = accounts_resp.json().get("accounts", [])
-if not accounts:
+coach_accounts = accounts_resp.json().get("accounts", [])
+print(coach_accounts) 
+if not coach_accounts:
     print("\n⚠️ No accounts returned.")
-    exit()
-account_id = accounts[1]["accountId"] # 0 is APITEST and 1 is USSS
-name = accounts[1]["name"]
-print("Using accountId:", account_id, 'from:', name)
+    exit()'''
 
 # =========================
-# STEP 2 — TEAMS
+# STEP 2 — TEAMS - right now commented out because I have my hard coded TEAM_ID
 # =========================
-teams_resp = test_endpoint(
+'''teams_resp = test_endpoint(
     "Teams",
-    f"{BASE_URL}/sports/accounts/{account_id}/teams"
+    f"{BASE_URL}/sports/accounts/{USSS_COACH_ID}/teams"
 )
 if teams_resp.status_code != 200:
     print("\n Cannot access teams — stop here.")
     exit()
 teams = teams_resp.json().get("teams", [])
+print(teams)
 if not teams:
     print("\n⚠️ No teams returned.")
-    exit()
-team_id = teams[0]["teamId"]
-print("Using teamId:", team_id)
+    exit()'''
 
 # =========================
 # STEP 3 — ATHLETES
 # =========================
 athletes_resp = test_endpoint(
-    "Athletes",
-    f"{BASE_URL}/sports/accounts/{account_id}/teams/{21426}/athletes"
+    "Athlete List",
+    f"{BASE_URL}/sports/accounts/{USSS_COACH_ID}/teams/{TEAM_ID}/athletes"
 )
 athletes = athletes_resp.json().get("athletes", [])
 print(f"Found {len(athletes)} athletes")
 
 # =========================
-# STEP 4 — SESSIONS (OPTIONAL DATE FILTER)
+# STEP 4 — GET ATHLETE MEASUREMENTS (think of as a session)
 # =========================
-from_time, to_time = last_x_days_utc_range(365)
-print(team_id)
-print(from_time, to_time)
-sessions_resp = test_endpoint(
-    "Sessions (all)",
-    f"{BASE_URL}/sports/accounts/{account_id}/teams/{team_id}/sessions", #TODO: using a test ID here so change it when needed
-    params={"fromTime": from_time, "toTime": to_time}
-)
-sessions = sessions_resp.json().get("sessions", [])
-print(f"Found {len(sessions)} sessions")
+
+# get measurementIds for athelte sessions in last x days
+from_time, to_time = last_x_days_utc_range(LAST_X_DAYS) # get time intervals for last day
+measurements = {}
+for athlete in athletes:
+    athlete_id = athlete['athleteId']
+    name = f"{athlete['firstName']} {athlete['lastName']}"
+
+    # Pull athlete 'measurements'
+    measurement_ids = get_measurement_ids(athlete_id, from_time, to_time, name='')
+    if measurement_ids != []:
+        measurements[athlete_id] = measurement_ids
+
+print(measurements)
+
+# get results of the measuremnts 
+rmssd = []
+athlete_w_measurements = list(measurements.keys())
+for athlete in athlete_w_measurements:
+    for measurement_id in measurements[athlete]:
+        resp = get_measurement_results(athlete, measurement_id)
+        print(resp)
+        rmssd.append(resp)
 
 # =========================
-# STEP 5 — SESSION RESULTS
+# STEP 5 — Measurement results RESULTS --> 
 # =========================
-for session in sessions:
-    session_id = session["sessionId"]
-    test_endpoint(
-        f"Session Results for {session_id}",
-        f"{BASE_URL}/sports/accounts/{account_id}/teams/{team_id}/sessions/{session_id}/results",
-        params={"var": "acwr"} 
-    )
+'''athlete_id = 570111          # River
+measurement_id = 14825086   # 12–14 quick recovery test
+
+measurement_resp = test_endpoint(
+    f"TEST: Measurement results (athlete {athlete_id} for measurement {measurement_id})",
+    f"{BASE_URL}/sports/accounts/{USSS_COACH_ID}/athletes/{athlete_id}/measurements/{measurement_id}/results",
+    params={
+        "format": "list",
+        # Optional: only request specific variables if you know them
+        # "var": "hr,rr,rmssd"
+    }
+)
+
+print(measurement_resp.text)'''
 
 print("\n=== DONE ===")
+
